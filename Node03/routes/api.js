95b1abfa3357ff7e19e82b78d861a59db95830c6
Hoai Nam <nhnnhn1832004@gmail.com>
2024-11-13 23:29:54 +0700

    post data api

--- Node03/routes/api.js
+++ Node03/routes/api.js
@@ -2,4 +2,6 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/api/v1/user.controller')
 router.get('/v1/users', userController.index)
+router.get('/v1/users/:id', userController.find)
+router.post('/v1/users', userController.store)
 module.exports = router;
